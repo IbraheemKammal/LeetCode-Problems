b73366694042edf199d7a879ba636ec5ba0f5171
Ibraheem <Ibraheemshammala@gmail.com>
2025-01-15 15:15:34 +0200

    Add project files.

---
+++ LeetCode-Problems/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSum twoSum = new TwoSum();
+            var result = twoSum.Do([98, 2, 4, 55, 232, 97, 3], 100);
+            foreach(in)
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ LeetCode-Problems/TwoSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Problems
+{
+    internal class TwoSum
+    {
+        public int[] Do(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            int firstIndex = 0, secondIndex;
+            for (; firstIndex < nums.Length; firstIndex++)
+            {
+                int current = nums[firstIndex];
+                int currentTargetReminder;
+                if (current > target) currentTargetReminder = current - target;
+                else currentTargetReminder = target - current;
+                secondIndex = Array.IndexOf(nums, currentTargetReminder);
+                if (secondIndex != -1) result = new int[] { firstIndex, secondIndex };
+            }
+            return result;
+
+        }
+    }
+}
